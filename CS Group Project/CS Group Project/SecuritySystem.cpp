73188fb148256400f3be060e0e48e90127e57ec1
JasonNgo97 <ngojason1997@gmail.com>
2014-05-31 10:45:02 -0700

    Update SecuritySystem.cpp

--- CS Group Project/CS Group Project/SecuritySystem.cpp
+++ CS Group Project/CS Group Project/SecuritySystem.cpp
@@ -7,3 +7,26 @@
 //
 
 #include "SecuritySystem.h"
+
+
+int SecuritySystem::checkusername(string inputusername)//this is probably wrong cuz it needs to take a parameter
+{
+    for(int x=0;x<MAXCOUNT;X++)
+    {
+      if (inputusername==username[x])
+      {
+        index=x;
+        return 0;//this will return 0 on success
+      }
+      return -1;//this will return this on failure
+    }
+}
+
+int SecuritySystem::checkpassword(string inputpassword)
+{
+  if(password[index]==inputpassword)
+  {
+    return 0;//succcess
+  }
+  return -1; //failure
+}
